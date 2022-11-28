0b359a1d1dd41b4ab4eb7b20c262ace327fe0655
Tgbotz123 <119308759+Tgbotz123@users.noreply.github.com>
2022-11-28 23:20:25 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -70,8 +70,7 @@ async def start(client, message):
         if message.command[1] != "subscribe":
             try:
                 kk, file_id = message.command[1].split("_", 1)
-                pre = 'checksubp' if kk == 'filep' else 'checksub' 
-                btn.append([InlineKeyboardButton(" 🔄 Try Again", callback_data=f"{pre}#{file_id}")])
+                btn.append([InlineKeyboardButton(" 🔄 Try Again", callback_data=f"checksub#{file_id}")])
             except (IndexError, ValueError):
                 btn.append([InlineKeyboardButton(" 🔄 Try Again", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
         await client.send_message(
